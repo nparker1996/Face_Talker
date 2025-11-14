2f24b262ca0de2b51761ad77b67bed63b82d1d30
Noah Parker <11395328+nparker1996@users.noreply.github.com>
2025-11-14 16:50:06 -0700

    Initial Setup of ChatGPT and Basic UI
    Added code for getting ChatGPT to communicate with Unity, work in progress
    
    Added Main Menu with buttons, setup calling a friend and quitting.

---
+++ Assets/Scripts/ChatFace.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class UnityStringEvent : UnityEvent<string> { }
+
+
+public class ChatFace : MonoBehaviour
+{
+    public UnityStringEvent onChatEvent;
+    public string inputString = "what is 6 times 7?";
+    public string prePrompt = "Respond briefly and help assess my mental & cognitive state, while assuming that I am elderly person.";
+
+    private void Start()
+    {
+        onChatEvent.AddListener(HandleStringEvent);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T)) {
+            onChatEvent.Invoke(prePrompt + " " + inputString);
+        }
+    }
+
+    public void HandleStringEvent(string message)
+    {
+        Debug.Log("Chat Received Message: " + message);
+    }
+}
