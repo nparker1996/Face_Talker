7a0225f513fd5372cc9c558199efde00a1dc01b6
Noah Parker <11395328+nparker1996@users.noreply.github.com>
2025-11-15 10:02:40 -0700

    Got ChatGPT communication and Readme setup
    updated OpenAI package used to OpenAI Unity which is now correctly talking with ChatGPT
    Did initial stubbing of ReadMe sections for future additions

--- Assets/Scripts/ChatFace.cs
+++ Assets/Scripts/ChatFace.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Concurrent;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
+using OpenAI;
 
 [System.Serializable]
 public class UnityStringEvent : UnityEvent<string> { }
@@ -9,24 +10,77 @@ public class UnityStringEvent : UnityEvent<string> { }
 
 public class ChatFace : MonoBehaviour
 {
-    public UnityStringEvent onChatEvent;
-    public string inputString = "what is 6 times 7?";
-    public string prePrompt = "Respond briefly and help assess my mental & cognitive state, while assuming that I am elderly person.";
+    [SerializeField] private InputField inputField;
+    [SerializeField] private Button button;
+    [SerializeField] private ScrollRect scroll;
+
+    [SerializeField] private RectTransform sent;
+    [SerializeField] private RectTransform received;
+    
+    private float height;
+    private OpenAIApi openai = new OpenAIApi();
+
+    private List<ChatMessage> messages = new List<ChatMessage>();
+    private string prompt = "Act as a care taker sitting right next to the user, have a converstation to gauge their mental & cognitive state. Don't break character. Don't ever mention that you are an AI model.";
 
     private void Start()
     {
-        onChatEvent.AddListener(HandleStringEvent);
+        button.onClick.AddListener(SendReply);
     }
 
-    private void Update()
+    private void AppendMessage(ChatMessage message)
     {
-        if (Input.GetKeyDown(KeyCode.T)) {
-            onChatEvent.Invoke(prePrompt + " " + inputString);
+        scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, 0);
+
+        var item = Instantiate(message.Role == "user" ? sent : received, scroll.content);
+        item.GetChild(0).GetChild(0).GetComponent<Text>().text = message.Content;
+        item.anchoredPosition = new Vector2(0, -height);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(item);
+        height += item.sizeDelta.y;
+        scroll.content.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, height);
+        scroll.verticalNormalizedPosition = 0;
+    }
+
+    private async void SendReply()
+    {
+        var newMessage = new ChatMessage()
+        {
+            Role = "user",
+            Content = inputField.text
+        };
+
+        AppendMessage(newMessage);
+
+        if (messages.Count == 0) newMessage.Content = prompt + "\n" + inputField.text;
+
+        messages.Add(newMessage);
+
+        button.enabled = false;
+        inputField.text = "";
+        inputField.enabled = false;
+
+        // Complete the instruction
+        var completionResponse = await openai.CreateChatCompletion(new CreateChatCompletionRequest()
+        {
+            Model = "gpt-4o-mini",
+            Messages = messages
+        });
+
+        if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
+        {
+            var message = completionResponse.Choices[0].Message;
+            message.Content = message.Content.Trim();
+
+            messages.Add(message);
+            AppendMessage(message);
         }
-    }
+        else
+        {
+            Debug.LogWarning("No text was generated from this prompt.");
+        }
 
-    public void HandleStringEvent(string message)
-    {
-        Debug.Log("Chat Received Message: " + message);
+        button.enabled = true;
+        inputField.enabled = true;
     }
+    
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Face_Talker
+# Face Talker
+A prototype assessment for Dreamface of an Android application that leverages speech-to-text (SST) and ChatGPT to roughly assess a user’s mental & cognitive state. 
+
+## How To Use
+
+## Architecture and Design
+
+## Noteable Packages
+- OpenAI Unity - https://github.com/srcnalt/OpenAI-Unity
+- Speech to Text - https://github.com/yasirkula/UnitySpeechToText
 
